f230b5311cb50b20c6a79a8028c0f78a836d03e2
Julien <julien.maffar@hotmail.com>
2024-03-27 16:57:59 +0100

    feat: ✨ create About section in Home Page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Poppins } from "next/font/google";
+import "@/styles/globals.scss";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='fr'>
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 }
