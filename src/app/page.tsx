e091df865698a48c6fd4a85d87373740ce06e865
Julien <julien.maffar@hotmail.com>
2024-03-28 15:56:49 +0100

    feat: ✨ add Services section inside HomePage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,88 @@
-import { Title } from "@/components";
+import { ServiceCard, Title } from "@/components";
 
 const Home = () => {
   return (
-    // About
-    <div className='section primary'>
-      <Title title='A propos' />
+    <div>
+      <section className='primary'>
+        <Title title='A propos' />
+        <p className='mt-14'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at
+          vestibulum diam, <strong>ut dictum risus</strong>. Vivamus accumsan
+          nibh eget nibh pretium tincidunt. Sed ipsum libero, scelerisque vel
+          ligula et, gravida sollicitudin ex. Etiam id rhoncus risus.
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at
+          vestibulum diam, ut dictum risus. Vivamus accumsan nibh eget nibh
+          pretium tincidunt. Sed ipsum libero, scelerisque vel ligula et,
+          gravida sollicitudin ex. Etiam id rhoncus risus.
+          <br />
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at
+          vestibulum diam, ut dictum risus. Vivamus accumsan nibh eget nibh
+          pretium tincidunt. Sed ipsum libero, scelerisque vel ligula et,
+          gravida sollicitudin ex. Etiam id rhoncus risus.
+          <br />
+          <br />
+          Vestibulum condimentum, nunc convallis sollicitudin iaculis, magna
+          lorem tincidunt metus, ac gravida ex felis quis elit. Mauris aliquet
+          massa eget interdum cursus. Ut semper turpis ut justo condimentum, non
+          pellentesque dolor faucibus. Fusce et dapibus justo. In hac habitasse
+          platea dictumst. Nunc turpis lorem, venenatis non rhoncus id,
+          imperdiet id erat. Mauris lorem urna, vehicula sed eros ac, dapibus
+          bibendum turpis. Suspendisse sagittis ullamcorper elit eget fringilla.
+          <br />
+          <br />
+          Vestibulum condimentum, nunc convallis sollicitudin iaculis, magna
+          lorem tincidunt metus, ac gravida ex felis quis elit. Mauris aliquet
+          massa eget interdum cursus. Ut semper turpis ut justo condimentum, non
+          pellentesque dolor faucibus. Fusce et dapibus justo. In hac habitasse
+          platea dictumst. Nunc turpis lorem, venenatis non rhoncus id,
+          imperdiet id erat. Mauris lorem urna, vehicula sed eros ac, dapibus
+          bibendum turpis. Suspendisse sagittis ullamcorper elit eget fringilla.
+        </p>
+      </section>
 
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at
-        vestibulum diam, <strong>ut dictum risus</strong>. Vivamus accumsan nibh
-        eget nibh pretium tincidunt. Sed ipsum libero, scelerisque vel ligula
-        et, gravida sollicitudin ex. Etiam id rhoncus risus.
-        <br />
-        <br />
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at
-        vestibulum diam, ut dictum risus. Vivamus accumsan nibh eget nibh
-        pretium tincidunt. Sed ipsum libero, scelerisque vel ligula et, gravida
-        sollicitudin ex. Etiam id rhoncus risus.
-        <br />
-        <br />
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at
-        vestibulum diam, ut dictum risus. Vivamus accumsan nibh eget nibh
-        pretium tincidunt. Sed ipsum libero, scelerisque vel ligula et, gravida
-        sollicitudin ex. Etiam id rhoncus risus.
-        <br />
-        <br />
-        Vestibulum condimentum, nunc convallis sollicitudin iaculis, magna lorem
-        tincidunt metus, ac gravida ex felis quis elit. Mauris aliquet massa
-        eget interdum cursus. Ut semper turpis ut justo condimentum, non
-        pellentesque dolor faucibus. Fusce et dapibus justo. In hac habitasse
-        platea dictumst. Nunc turpis lorem, venenatis non rhoncus id, imperdiet
-        id erat. Mauris lorem urna, vehicula sed eros ac, dapibus bibendum
-        turpis. Suspendisse sagittis ullamcorper elit eget fringilla.
-        <br />
-        <br />
-        Vestibulum condimentum, nunc convallis sollicitudin iaculis, magna lorem
-        tincidunt metus, ac gravida ex felis quis elit. Mauris aliquet massa
-        eget interdum cursus. Ut semper turpis ut justo condimentum, non
-        pellentesque dolor faucibus. Fusce et dapibus justo. In hac habitasse
-        platea dictumst. Nunc turpis lorem, venenatis non rhoncus id, imperdiet
-        id erat. Mauris lorem urna, vehicula sed eros ac, dapibus bibendum
-        turpis. Suspendisse sagittis ullamcorper elit eget fringilla.
-      </p>
+      <section className='secondary'>
+        <Title title='Services' />
+
+        <div
+          className='flex flex-wrap justify-between mt-14'
+          style={{ gap: 36 }}>
+          <ServiceCard
+            firstIcon='next'
+            secondIcon='react'
+            title='Frontend JS'
+            subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id justo tortor. Aenean vel ante ut tellus tempus tempus.'
+          />
+          <ServiceCard
+            firstIcon='adonis'
+            secondIcon='nest'
+            title='Backend JS'
+            subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id justo tortor. Aenean vel ante ut tellus tempus tempus.'
+          />
+          <ServiceCard
+            firstIcon='laravel'
+            title='Backend Laravel'
+            subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id justo tortor. Aenean vel ante ut tellus tempus tempus.'
+          />
+          <ServiceCard
+            firstIcon='wordpress'
+            title='Wordpress'
+            subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id justo tortor. Aenean vel ante ut tellus tempus tempus.'
+          />
+          <ServiceCard
+            firstIcon='teacher'
+            title='Formations'
+            subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id justo tortor. Aenean vel ante ut tellus tempus tempus.'
+          />
+          <ServiceCard
+            firstIcon='figma'
+            title='Design UX/UI'
+            subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla id justo tortor. Aenean vel ante ut tellus tempus tempus.'
+          />
+        </div>
+      </section>
     </div>
   );
 };
